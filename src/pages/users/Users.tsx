30e0b9e49fe40d538164fbd315e18348ae45ec1b
Adrian <add.rinputra@gmail.com>
2024-12-23 13:56:00 +0700

    Adding Modal and add new item function to the user page

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -2,11 +2,13 @@ import { GridColDef } from '@mui/x-data-grid';
 import DataTable from '../../components/dataTable/DataTable'
 import './users.scss'
 import { userRows } from '../../data';
+import { useState } from 'react';
+import Add from '../../components/add/Add';
 
 const columns: GridColDef[] = [
   { field: 'id', headerName: 'ID', width: 90 },
   {
-    field: 'avatar', 
+    field: 'img', 
     headerName:'Avatar', 
     width: 100,
     renderCell: (params) => {
@@ -39,6 +41,7 @@ const columns: GridColDef[] = [
   },
   {
     field: 'createdAt',
+    type: 'string',
     headerName: 'Created At',
     width: 200,
   },
@@ -51,13 +54,16 @@ const columns: GridColDef[] = [
 ];
 
 const Users = () => {
+  const [open, setOpen] = useState(false)
+
   return (
     <div className="users">
       <div className="info">
         <h1>Users</h1>
-        <button>Add New User</button>
+        <button onClick={() => setOpen(true)}>Add New User</button>
       </div>
       <DataTable slug='users' columns={columns} rows={userRows} />
+      {open && <Add slug='user' columns={columns} setOpen={setOpen}/>}
     </div>
   )
 }
